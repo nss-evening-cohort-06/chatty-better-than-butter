ff1ff155cb9133faa82bd21d26d5993dc1d6cb66
Waamber <amberastuart@aol.com>
2017-09-20 16:47:18 -0500

    added comments

--- javascripts/events.js
+++ javascripts/events.js
@@ -7,7 +7,6 @@ let messageBoard = document.getElementById("message-board");
 
 //deleteBtn when clicked will find the parent "messageCard" and delete it.
 messageBoard.addEventListener("click", (event) => {
-
   messages.messageDelete(event.target.parentNode.children[0].innerHTML);
-  console.log(event.target.parentNode.children[0].innerHTML);
+  // console.log(event.target.parentNode.children[0].innerHTML);
 });
