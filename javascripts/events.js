313825a40024d2ab18dad0bc02a6441fcc2216d7
Waamber <amberastuart@aol.com>
2017-09-20 19:52:59 -0500

    added event listener to change themes

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,12 +1,27 @@
 "use strict";
 
-let domString = require("./dom");
-let messages = require("./xhr");
+const domString = require("./dom");
+const messages = require("./xhr");
+const themes = require("./themes");
 
-let messageBoard = document.getElementById("message-board");
+const messageBoard = document.getElementById("message-board");
+const themeRadios = document.getElementById("radioThemes");
+const body = document.getElementById("body");
 
 //deleteBtn when clicked will find the parent "messageCard" and delete it.
 messageBoard.addEventListener("click", (event) => {
   messages.messageDelete(event.target.parentNode.children[0].innerHTML);
   // console.log(event.target.parentNode.children[0].innerHTML);
 });
+
+
+//loops through radio buttons and removes css class to add different css class
+themeRadios.addEventListener("change", (event) => {
+  for (let i = 0; i < themeRadios.children.length; i++) {
+    if (event.target.id === "bob") {
+      body.classList.remove("red");
+      body.classList.add("changeBackground");
+    }
+  }
+});
+
---
+++ javascripts/themes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// const themes = require("./events");
+
+// const bob = document.getElementById("bob");
+// const catty = document.getElementById("catty");
+// const starwars = document.getElementById("starwars");
+// const magical = document.getElementById("magical");
+// const body = document.getElementById("body");
+
+
+
