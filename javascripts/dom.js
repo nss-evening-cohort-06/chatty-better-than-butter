4c200b62e416103b885cb3fe5afde93be9cd85d1
Waamber <amberastuart@aol.com>
2017-09-22 10:45:22 -0500

    cat theme (temporary main)

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,15 +1,9 @@
 "use strict";
 
-
-
-
-let xhr = require("./xhr");
-
-
 const domString = (messages) => {
-  var messageString = "";
+  let messageString = "";
   for (let i = 0; i < messages.length; i++) {
-    messageString += `<div class="messageCard" id="messageCard">
+    messageString += `<div class="messageCard">
                         <p>${messages[i].message}</p>
                         <button class="deleteBtn">Delete</button>
 									    </div > `;
@@ -19,7 +13,6 @@ const domString = (messages) => {
 
 const writeToDom = (strang) => {
   let messageContainer = document.getElementById("message-board");
-
   messageContainer.innerHTML = strang;
 };
 
