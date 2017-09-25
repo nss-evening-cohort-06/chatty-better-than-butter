529decf74268d7f5a9a9aeccfd10e57d588c153a
Jess Brawner <jessnbillbrawner@gmail.com>
2017-09-25 13:04:28 -0500

    added functionality to auto scroll to last message entered at bottom of message-board

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -3,18 +3,33 @@
 const domString = (messages) => {
   let messageString = "";
   for (let i = 0; i < messages.length; i++) {
-    messageString += `<div class="messageCard">
+    // Assign ID to last message posted only (will be used for scrolling to last message view):
+    if(i === messages.length -1){
+      messageString += `<div id="lastMessage" class="messageCard">
                         <p class="timestamp">${messages[i].timeStamp}</p>
                         <p id="message">${messages[i].message}</p>
                         <button class="deleteBtn">Delete</button>
-									    </div > `;
-  }
+                      </div > `;
+    }
+    else{
+      messageString += `<div class="messageCard">
+                          <p class="timestamp">${messages[i].timeStamp}</p>
+                          <p id="message">${messages[i].message}</p>
+                          <button class="deleteBtn">Delete</button>
+  									    </div > `;
+      }
+    }
   writeToDom(messageString);
 };
 
 const writeToDom = (strang) => {
   let messageContainer = document.getElementById("message-board");
   messageContainer.innerHTML = strang;
+  // Scroll to last message entered:
+  let lastMessage = document.getElementById("lastMessage");
+  const targetOffset = $('#lastMessage').offset();
+  const divScrollTop = $('#message-board').scrollTop();
+  $('#message-board').scrollTop(divScrollTop + targetOffset.top - $('#lastMessage').height());
 };
 
 module.exports = { domString, writeToDom };
